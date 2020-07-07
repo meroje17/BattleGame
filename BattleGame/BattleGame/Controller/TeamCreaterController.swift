2560239af92a5f30ab001db7da4a515be120e909
Jérôme Guèrin <guerinjerome@outlook.fr>
2020-07-07 15:58:46 +0200

    feat(controller): adding creater team feature with TeamCreaterController

--- BattleGame/BattleGame/Controller/TeamCreaterController.swift
+++ BattleGame/BattleGame/Controller/TeamCreaterController.swift
@@ -8,23 +8,90 @@
 
 import UIKit
 
-class TeamCreaterController: UIViewController {
+final class TeamCreaterController: UIViewController {
 
+    @IBOutlet private weak var chooseYourTeamLabel: UILabel!
+    @IBOutlet private var namesTextField: [UITextField]!
+    @IBOutlet private var racesSegmentedControl: [UISegmentedControl]!
+    @IBOutlet private weak var launchBattleButton: UIButton!
+    
+    typealias completion = () -> ()
+    private var playerIndex = 0
+    
+    @IBAction func tapLaunchBattleButton() {
+        if emptyTextVerification() {
+            createTeam {
+                if playerIndex == 0 {
+                    chooseYourTeamLabel.text = "Player 2 choose your team !"
+                    resetUI()
+                    playerIndex += 1
+                } else {
+                    performSegue(withIdentifier: "tapLaunchBattleButton", sender: nil)
+                }
+            }
+        } else { return }
+    }
+    
+    @objc func resignTextField() {
+        for name in namesTextField {
+            name.resignFirstResponder()
+        }
+    }
+    
+    private func initializer() {
+        launchBattleButton.layer.cornerRadius = launchBattleButton.bounds.height / 2
+        
+        for name in namesTextField {
+            name.delegate = self
+        }
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(resignTextField)))
+    }
+    
+    private func emptyTextVerification() -> Bool {
+        for name in namesTextField {
+            if name.text == "" || name.text == nil { error(with: .textEmpty, shake: launchBattleButton); return false }
+        }
+        return true
+    }
+    
+    private func createTeam(_ completion: completion) {
+        var index = 0
+        for race in racesSegmentedControl {
+            switch race.selectedSegmentIndex {
+            case 0:
+                Player.list[playerIndex].characters.append(Knight(namesTextField[index].text!))
+            case 1:
+                Player.list[playerIndex].characters.append(Ork(namesTextField[index].text!))
+            case 2:
+                Player.list[playerIndex].characters.append(Wizard(namesTextField[index].text!))
+            default:
+                return
+            }
+            index += 1
+        }
+        completion()
+    }
+    
+    private func resetUI() {
+        for name in namesTextField {
+            name.text = nil
+        }
+        for race in racesSegmentedControl {
+            race.selectedSegmentIndex = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initializer()
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension TeamCreaterController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        resignTextField()
+        return true
     }
-    */
-
 }
